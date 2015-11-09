2dcc33907b1ad22eed7e22dccb341465d8def912
MenZil <menzil@live.cn>
2015-11-09 13:55:13 +0800

    写一个函数，将两个字符串连接。

---
+++ concatenate-string.c
@@ -0,0 +1,29 @@
+#include "stdio.h"
+/*
+*写一个函数，将两个字符串连接。
+*/
+int main(int argc, char const *argv[])
+{
+	void concatenate(char string1[],char string2[],char string[]);
+	char s1[100],s2[100],s[100];
+	printf("input string1:\n");
+	scanf("%s",s1);
+	printf("input string2:\n");
+	scanf("%s",s2);
+	concatenate(s1,s2,s);
+	printf("\n The new string is: %s\n",s);
+	return 0;
+}
+
+void concatenate(char string1[],char string2[],char string[]){
+	int i,j;
+	for ( i = 0; string1[i] != '\0'; i++)
+	{
+		string[i] = string1[i];
+	}
+	for ( j = 0; string2[j] != '\0'; j++)
+	{
+		string[i+j] = string2[j];
+	}
+	string[i+j] = '\0';
+}
